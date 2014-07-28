45d7159d1bcceb82d479d22f35b285d3d0904b32
Ryuta Oshikiri <surfindaeac@gmail.com>
2014-07-28 12:08:40 -0700

    style change

--- ember-shortcuts.js
+++ ember-shortcuts.js
@@ -138,7 +138,7 @@
 
   function targetIsNotInput(event) {
     var tagName = event.target.tagName;
-    return !(tagName === 'INPUT' || tagName === 'SELECT' || tagName === 'TEXTAREA');
+    return !(tagName === 'INPUT') && !(tagName === 'SELECT') && !(tagName === 'TEXTAREA');
   }
 
   Ember.Shortcuts = Ember.Object.extend({
